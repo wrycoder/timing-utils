0951b069a6123d5d3e918303335ed4228176e0b6
Michael Toulouse <wrycoder@ameritech.net>
2024-03-24 11:07:43 +0000

    Adding structs for folder and hour type

--- include/Globals.h
+++ include/Globals.h
@@ -5,3 +5,18 @@
 /* Global instance handle */
 extern HINSTANCE g_hInstance;
 
+void ReportError(HWND hWnd, DWORD dwErr);
+
+typedef struct {
+  LPWSTR displayString;
+  LPWSTR directoryName;
+} DayInfo;
+
+extern const DayInfo days[];
+
+typedef struct {
+  LPWSTR hourName;
+  LPWSTR displayString;
+} HourInfo;
+
+extern const HourInfo hours[];
